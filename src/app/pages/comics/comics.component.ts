9f7770ab0e7ae170e8f1fecd0c2df6929fba5183
camilomendez1705 <cmendez1705@gmail.com>
2021-01-31 00:57:16 -0300

    se hacen los filtros para comic y prliculas y se agregan los estiolos para tajetas

--- src/app/pages/comics/comics.component.ts
+++ src/app/pages/comics/comics.component.ts
@@ -10,13 +10,31 @@ import { ComicsService } from 'src/app/_service/comics.service';
 })
 export class ComicsComponent implements OnInit {
  comics:Comic[];
+ nomComic:string;
+ tomComic:string;
+ argComic:string;
+ dataSource = new Array();
   constructor(private comicService:ComicsService, public snackBar: MatSnackBar) { }
 
   ngOnInit() {
     this.listarComics();
   }
+
    listarComics(){
      this.comicService.listar()
-     .subscribe(data => this.comics =data)
-   }
+     .subscribe(data => {
+    this.comics.forEach(lista =>{
+      this.nomComic = lista.nombreComic;
+      this.tomComic = lista.tomoComic;
+      this.argComic = lista.argumentoComic;
+    });
+    this.comics = data
+});
+}
+filtrar(event){
+  const texto = event.target.value.toString().toLowerCase().trim();
+  this.comics = [];
+  this.dataSource = this.comics;
+  this.dataSource.filter = texto;
+}
 }
